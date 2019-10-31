e87d2f004ec9b0ff1ab0b6142b58aee18960a2c4
hisaotoriyama <hisao.toriyama@gmail.com>
2019-10-31 22:26:07 +0900

    20191031

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ app.use(express.static('public'));
 console.log("test")
 // start application
 app.listen(3000)
+/*
+var https = require('https');
+var fs = require('fs');
+var ssl_server_key = 'server_key.pem';
+var ssl_server_crt = 'server_crt.pem';
+var port = 3000;
+ 
+var options = {
+        key: fs.readFileSync(ssl_server_key),
+        cert: fs.readFileSync(ssl_server_crt)
+};
+ 
+let server = https.createServer(options, app)
+server.listen(port);
+*/
--- public/qrreader.js
+++ public/qrreader.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: "#app",
     data: {
-        QRData: "Deruzoderuzo"
+        QRData: ""
     },
     methods: {
         dataexport: function() {

69d4defb2892cc9035b07f04e87f2e5710cd750f
hisaotoriyama <hisao.toriyama@gmail.com>
2019-11-12 20:26:09 +0900

    20191110

--- public/qrreader.js
+++ public/qrreader.js
@@ -4,7 +4,31 @@ var app = new Vue({
         globalData: globalData
     },
     methods: {
-        dataexport: function() {
-            console.log(this.globalData.QRdata)
-      }
-    }})
+        locsdataexport: function() {
+            console.log(JSON.stringify(this.globalData.QRdata))
+                const data = {
+                  "storageloc": this.globalData.QRdata.storageloc
+                };
+                const headers = {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                };
+                const d = {
+                headers: headers,
+                method: "PUT",
+                body: JSON.stringify(data) 
+                };
+          
+                var self = this;
+                fetch('/adduser', d).then((res) =>  
+                    // res.redirect('../private/todo.html',this.newName)
+                    location.href = res.url
+                    )  
+                
+                this.newName = "";
+                this.newPassword="";
+              },
+
+        docsdataexport: function() {
+      }}
+    })
